962355b89a381274017467da6885dae4d230bea1
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-05-19 08:05:24 +0000

    Improve example

--- samples/Basic/Blink/BlinkSample.cpp
+++ samples/Basic/Blink/BlinkSample.cpp
@@ -1,12 +1,17 @@
 #include "BlinkSample.h"
 
+using namespace std;
+using namespace codal;
+
 void BlinkSample_main(codal::STM32STEAM32_WB55RG& steam32)
 {
     bool state = false;
+
     while (true) {
         steam32.io.PC_10.setDigitalValue((int)state);
-        // steam32.io.led2.setDigitalValue((int)!state);
-        steam32.sleep(1000);
+        steam32.io.PC_12.setDigitalValue((int)!state);
+
+        steam32.sleep(500);
         state = !state;
     }
 }
