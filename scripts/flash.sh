eabcab731eaf9409fd0a86be6b4c26f59ac2dd0b
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2024-09-10 12:08:38 +0000

    Make uniform debug scripts

--- scripts/flash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-./build.py
-openocd -s "/usr/share/openocd/scripts/" -s"libraries/codal-stm32-PNUCLEO_WB55RG/scripts/" -f "openocd.cfg" -c "program PNUCLEO_WB55RG.hex verify reset exit "
--- scripts/gdb.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-arm-none-eabi-gdb --eval "target remote | openocd -s /usr/share/openocd/scripts/ -s libraries/codal-stm32-PNUCLEO_WB55RG/scripts/ -f openocd.cfg -f debug.cfg" build/PNUCLEO_WB55RG
