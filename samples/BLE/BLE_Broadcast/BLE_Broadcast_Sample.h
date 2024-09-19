3fb31680e27101caf1d2574e213940e11736466d
Jonathan BAUDIN <jjbaudin@gmail.com>
2024-09-19 15:13:18 +0000

    Add BLE Advertising sample

---
+++ samples/BLE/BLE_Broadcast/BLE_Broadcast_Sample.h
@@ -0,0 +1,12 @@
+#include "STM32STEAM32_WB55RG.h"
+
+#ifndef BLE_BROADCAST_SAMPLE_H
+#define BLE_BROADCAST_SAMPLE_H
+
+#ifndef SAMPLE_MAIN
+#define SAMPLE_MAIN BLE_Broadcast_Sample_main
+#endif
+
+void BLE_Broadcast_Sample_main(codal::STM32STEAM32_WB55RG& steam32);
+
+#endif
